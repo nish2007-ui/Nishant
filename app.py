81e03ad5bdcdbf971924511a210c05e4dbf37a13
nish2007-ui <sumitapatel7588@gmail.com>
2026-07-16 19:55:01 +0530

    Update PHOTO_URL to use raw GitHub link

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ BIO = (
 # Example: if your repo is github.com/yourname/portfolio and the file is
 # assets/photo.jpg on the main branch, the raw URL is:
 # https://raw.githubusercontent.com/yourname/portfolio/main/assets/photo.jpg
-PHOTO_URL = "https://github.com/nish2007-ui/Nishant/blob/b4100989dd4012c14cdc596fad7ce94a1d2a0269/paasport.jpg"
+PHOTO_URL = "https://raw.githubusercontent.com/Nishant/portfolio/paasport.jpg"
 
 CITY = ""              # TODO: add your city
 STUDENT_STATUS = ""    # TODO: e.g. "Undergraduate, IIT Bombay" or "2nd Year B.Tech"
